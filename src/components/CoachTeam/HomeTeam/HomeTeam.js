75035cf84ebfc307c7a7f29fc642a5b4f24dfc0f
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-22 16:09:40 -0500

    added conditional render of new run button

--- src/components/CoachTeam/HomeTeam/HomeTeam.js
+++ src/components/CoachTeam/HomeTeam/HomeTeam.js
@@ -3,6 +3,17 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class HomeTeam extends Component {
+
+  renderNewRunButton = () => {
+    // if team has access to create their own runs
+    if ( this.props.user.security_clearance === 4 ) {
+      // return button for routing to start of new run view
+      return (
+        <Link to="/practice-run" ><button>Create New Run</button></Link>
+      );
+    }
+  }
+
   render () {
     return (
         <div>
@@ -11,8 +22,8 @@ class HomeTeam extends Component {
           <Link to="/missions" ><button>View Missions</button></Link>
           {/* button to route to run history view */}
           <Link to="/history" ><button>View Run History</button></Link>
-          {/* button to route to start of new run view */}
-          <Link to="/practice-run" ><button>Create New Run</button></Link>
+          {/* conditionally render button based on user clearance level */}
+          { this.renderNewRunButton() }
         </div>
 		);
   }
