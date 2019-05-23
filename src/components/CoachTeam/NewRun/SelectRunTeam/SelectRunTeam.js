0622351a5147faa6ae4a932717cbf773aa4fcade
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-05-23 13:33:06 -0500

    created maps for selects, created state

--- src/components/CoachTeam/NewRun/SelectRunTeam/SelectRunTeam.js
+++ src/components/CoachTeam/NewRun/SelectRunTeam/SelectRunTeam.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SelectRunTeam extends Component {
+    state = {
+        driver: '',
+        assistant: '',
+        scorekeeper: ''
+    }
     render(){
         return(
             <div>
                 <form>
                     <label>Driver:</label>
-                    <input type='select' />
+                    <select>
+                        {this.props.reduxState.allTeams.map((teamMember, i) => 
+                            <option value={teamMember.name} key={i}>{teamMember.name}</option>
+                        )}
+                    </select>
                     <label>Assistant:</label>
-                    <input type='select' />
+                    <select>
+                        {this.props.reduxState.allTeams.map((teamMember, i) =>
+                            <option value={teamMember.name} key={i}>{teamMember.name}</option>
+                        )}
+                    </select>
                     <label>Scorekeeper:</label>
-                    <input type='select' />
+                    <select>
+                        {this.props.reduxState.allTeams.map((teamMember, i) =>
+                            <option value={teamMember.name} key={i}>{teamMember.name}</option>
+                        )}
+                    </select>
                 </form>
             </div>
         )
     }
 }
+
+const mapReduxStateToProps = reduxState => ({
+    reduxState,
+})
+
+export default connect(mapReduxStateToProps)(SelectRunTeam);
