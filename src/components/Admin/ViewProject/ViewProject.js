60ebbe76e5d9a1aa374f52d47647de93df73f997
Bradley Hennen <bradhennen5@gmail.com>
2019-05-24 11:19:26 -0500

    printing either/or goals to the DOM

--- src/components/Admin/ViewProject/ViewProject.js
+++ src/components/Admin/ViewProject/ViewProject.js
@@ -21,7 +21,7 @@ class ViewProject extends Component {
         this.props.dispatch({ type: 'GET_PROJECT_DETAILS', payload: searchObject });
         this.props.dispatch({ type: 'GET_PENALTIES', payload: searchObject });
         this.props.dispatch({ type: 'GET_MISSIONS', payload: searchObject });
-        this.props.dispatch({ type: 'GET_EITHER_OR', payload: searchObject});
+        this.props.dispatch({ type: 'GET_EITHER_OR', payload: searchObject });
     }
 
     componentDidUpdate(prevProps) {
@@ -55,44 +55,58 @@ class ViewProject extends Component {
         //Find a way to stop loop other than #100
         for (let count = 0; count < 100; count++) {
             test = missionArr.filter(x => x.mission_id == count)
-            
-            if (test.length !== 0 ) {
-            console.log('test length', test.length);
+
+            if (test.length !== 0) {
+                console.log('test length', test.length);
 
                 newArr.push(test)
-            }       
-            
+            }
+
         }
         console.log('newArr', newArr);
         return (
-                newArr.map( (mission, i)  => {
-                    return (
-                        <div key={i}>
-                            <h3>Mission {i + 1}: {mission[0].mission_name}</h3>
-                            <button>Edit</button>
-                            <button>Delete</button>
-                            <h4>{mission[0].description}</h4>
-                            {mission.map( mission => {
-                                return (
-                                    <div>
-                                        {this.renderGoals(mission)}
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    )
-                })   
+            newArr.map((mission, i) => {
+                return (
+                    <div key={i}>
+                        <h3>Mission {i + 1}: {mission[0].mission_name}</h3>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                        <h4>{mission[0].description}</h4>
+                        {mission.map(mission => {
+                            return (
+                                <div>
+                                    {this.renderGoals(mission)}
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            })
         )
     }
 
     renderGoals = (mission) => {
         if (mission.goal_type_id === 1) {
-           return <h5>Goal: {mission.name} = {mission.points} points</h5>
+            return <h5>Goal: {mission.name} = {mission.points} points</h5>
         }
         else if (mission.goal_type_id === 2) {
-            return <h5>Working on it</h5>
+            return (
+                this.state.projectEitherOr.map( either => {
+                    console.log('mission.goal_id', mission.goal_id);
+                    console.log('either.goal_id', either.goal_id);
+                    
+                    if (mission.goal_id == either.goal_id) {
+                        return (
+                            <div>
+                                <h5>Goal: {either.name} = {either.points} points</h5>
+                            </div>
+                        )
+                    }
+                })
+            )
         }
         else if (mission.goal_type_id === 3) {
+
             return (
                 <div>
                     <h5>Goal: {mission.name} = {mission.points} points each</h5>
@@ -109,6 +123,8 @@ class ViewProject extends Component {
                 {/* {JSON.stringify(this.state.projectPenalties)} */}
                 {/* {JSON.stringify(this.state.projectDetails)} */}
                 {JSON.stringify(this.state.projectEitherOr)}
+                <h1></h1>
+                {JSON.stringify(this.state.projectMissions)}
                 <h1>{this.state.projectDetails.name}</h1>
                 <h2>The Project</h2>
                 <p>{this.state.projectDetails.description}</p>
@@ -136,7 +152,7 @@ class ViewProject extends Component {
                     <hr />
                     {this.groundControl()}
                 </div>
-               
+
             </div>
         )
     }
