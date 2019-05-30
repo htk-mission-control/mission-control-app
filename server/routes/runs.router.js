351f9dde8b348ef158e61fafeaa5e6f5e2755076
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-30 11:17:57 -0500

    changed sql text

--- server/routes/runs.router.js
+++ server/routes/runs.router.js
@@ -17,35 +17,55 @@ pool.query( sqlText )
 }); // end transaction
 
 /**
- * GET runs for coach
+ * GET runs for coach based on url query string
  */
-router.get('/coach', (req, res) => {
+router.get('/coach/:id', (req, res) => {
+
+    let id = req.params.id;
+
     const sqlText = `
         SELECT "runs"."id", "runs"."name", COUNT(CASE WHEN "goals_per_run"."is_completed" THEN 1 end), "runs"."score" FROM "runs"
         JOIN "selected_missions" ON "run_id" = "runs"."id"
         JOIN "goals_per_run" ON "selected_missions_id" = "selected_missions"."id"
+        WHERE "team_id" = $1
         GROUP BY "runs"."id";
-    `
-pool.query( sqlText )
-    .then ( result => {
-        console.log(result.rows);
-        res.send( result.rows );
-    }).catch ( error => {
-        res.sendStatus( 500 );
-    })
+    `;
+
+    pool.query( sqlText, [id] )
+        .then ( result => {
+            // result should be an array of objects with run information
+            // run id, run name, goals completed count, run score
+            res.send( result.rows );
+        }).catch ( error => {
+            res.sendStatus( 500 );
+        });
 });
 
 /**
- * GET runs for team
+ * GET runs for team based on user id
  */
 router.get('/team', (req, res) => {
-    const sqlText = ``
-pool.query( sqlText )
-    .then ( result => {
-        res.send( result.rows );
-    }).catch ( error => {
-        res.sendStatus( 500 );
-    })
+
+    let id = req.user.id;
+
+    const sqlText = `
+            SELECT "runs"."id", "runs"."name", COUNT(CASE WHEN "goals_per_run"."is_completed" THEN 1 end), "runs"."score" 
+            FROM "runs"
+            JOIN "selected_missions" ON "run_id" = "runs"."id"
+            JOIN "goals_per_run" ON "selected_missions_id" = "selected_missions"."id"
+            JOIN "teams" ON "teams"."id" = "runs"."team_id"
+            WHERE "teams"."team_user_id" = $1
+            GROUP BY "runs"."id";
+        `;
+
+    pool.query( sqlText, [id] )
+        .then ( result => {
+            // result should be an array of objects with run information
+            // run id, run name, goals completed count, run score
+            res.send( result.rows );
+        }).catch ( error => {
+            res.sendStatus( 500 );
+        });
 });
 
 module.exports = router;
