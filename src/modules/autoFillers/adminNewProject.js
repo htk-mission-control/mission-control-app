38cefa0924c1b6c0624584e6e83ab55eab841113
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-06-01 21:36:03 -0500

    editted description

--- src/modules/autoFillers/adminNewProject.js
+++ src/modules/autoFillers/adminNewProject.js
@@ -1,6 +1,6 @@
 let newProject = {
   name: 'Into Orbit',
-  description: 'Have you ever thought about what',
+  description: 'Have you ever thought about what it would be like to live in space for a year or more? What would you need to stay alive, healthty, and happy? The project challenge this season is to identify a human problem (physical or social) faced during long duration space explortion and develop an innovative and creative solution.',
   year: '2018',
   published: false,
 }
