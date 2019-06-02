3dee0b6c5947be5afeafec5cd7a27a10f2e6d1fd
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-06-01 21:53:30 -0500

    init

---
+++ src/modules/autoFillers/admin/addPenalty.js
@@ -0,0 +1,9 @@
+let newPenalty = {
+  project_id: 1,
+  name: 'Touch Penalty',
+  description: 'The kids should not touch the robot during their run. If they do, 1 penalty will be applied to their score.',
+  max: 7,
+  points: -2
+}
+
+module.exports = newPenalty;
